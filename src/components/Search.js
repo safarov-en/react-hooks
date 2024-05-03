fdf07ce9a722200c70432681d1439f980d8a8f11
safarov-en <safarov.elkhan110@gmail.com>
2024-05-03 14:11:59 +0500

    Added a search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,28 @@
+import { useContext, useState } from "react";
+import { AlertContext } from "../context/alert/alertContext";
+
 export const Search = () => {
+    const [value, setValue] = useState('');
+    const {show} = useContext(AlertContext);
+    const onSubmit = event => {
+        if(event.key !== 'Enter') {
+            return
+        }
+        if(value.trim()) {
+
+        } else {
+            show('Введите данные пользователя!');
+        }
+    }
     return (
         <div className="form-group">
             <input
                 type="text"
                 className="form-control"
                 placeholder="Введите ник пользователя..."
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={onSubmit}
             />
         </div>
     )
