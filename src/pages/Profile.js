4421465d09f3a024992ded45f9e53cfab6f4bcbe
safarov-en <safarov.elkhan110@gmail.com>
2024-05-03 16:23:50 +0500

    Information output

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,62 @@
+import { useContext, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { GithubContext } from "../context/github/githubContext";
+
 export const Profile = () => {
+    const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
+    const { name: urlName } = useParams();
+    useEffect(() => {
+        getUser(urlName);
+        getRepos(urlName);
+    }, [])
+    if(loading) {
+        return <p className="text-center">Загрузка...</p>
+    }
+    console.log(user)
+    const {
+        name, company, avatar_url,
+        location, bio, blog,
+        login, html_url, followers,
+        following, public_repos, public_gists
+    } = user;
     return (
-        <div>
-            <h1>Profile page</h1>
-        </div>
+        <>
+            <Link to="/" className="btn btn-link">На главную</Link>
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img src={avatar_url} style={{width: '150px'}} />
+                            <h1>{name}</h1>
+                            {location && <p>Местоположение: {location}</p>}
+                        </div>
+                        <div className="col">
+                            {
+                                bio && <>
+                                    <h3>BIO</h3>
+                                    <p>{bio}</p>
+                                </>
+                            }
+                            <a href={html_url} target="_blank" className="btn btn-dark">Открыть профиль</a>
+                            <ul>
+                                {login && <li>
+                                    <strong>Username: </strong> {login}
+                                </li>}
+                                {company && <li>
+                                    <strong>Компания: </strong> {company}
+                                </li>}
+                                {blog && <li>
+                                    <strong>Website: </strong> {blog}
+                                </li>}
+                            </ul>
+                            <div className="badge badge-primary">Подписчики: {followers}</div>
+                            <div className="badge badge-success">Подписан: {following}</div>
+                            <div className="badge badge-info">Репозиторий: {public_repos}</div>
+                            <div className="badge badge-dark">Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
