16218a4d3db48f08056cb71400dacbd141983498
safarov-en <safarov.elkhan110@gmail.com>
2024-05-03 15:12:23 +0500

    Output to the main page

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,16 +4,18 @@ import { GithubContext } from "../context/github/githubContext";
 
 export const Search = () => {
     const [value, setValue] = useState('');
-    const {show} = useContext(AlertContext);
+    const alert = useContext(AlertContext);
     const github = useContext(GithubContext);
     const onSubmit = event => {
         if(event.key !== 'Enter') {
             return
         }
+        github.clearUsers();
         if(value.trim()) {
+            alert.hide();
             github.search(value.trim());
         } else {
-            show('Введите данные пользователя!');
+            alert.show('Введите данные пользователя!');
         }
     }
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
-import { Search } from "../components/Search"
-import { Card } from "../components/Card"
+import { Search } from "../components/Search";
+import { Card } from "../components/Card";
+import { useContext } from "react";
+import { GithubContext } from "../context/github/githubContext";
 
 export const Home = () => {
+    const {loading, users} = useContext(GithubContext);
     return (
         <>
             <Search />
             <div className="row mt-4">
-                <div className="col-sm-4 mb-4">
-                    <Card />
-                </div>
-                <div className="col-sm-4 mb-4">
-                    <Card />
-                </div>
-                <div className="col-sm-4 mb-4">
-                    <Card />
-                </div>
+                {
+                    loading
+                        ? <p className="text-center">Загрузка...</p>
+                        : users.map(user => (<div className="col-sm-4 mb-4" key={user.id}>
+                                <Card user={user} />
+                            </div>)
+                        )
+                }
             </div>
         </>
     )
