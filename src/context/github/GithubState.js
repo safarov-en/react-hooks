8f11223b14a55786ba493a107c5a1fe75cdddf2d
safarov-en <safarov.elkhan110@gmail.com>
2024-05-03 14:40:40 +0500

    Adding methods

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import { GithubContext } from "./githubContext";
 import { githubReducer } from "./githubReducer";
+import { GET_USER, SEARCH_USERS, CLEAR_USERS, SET_LOADING, GET_REPOS } from "../types";
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -10,9 +11,34 @@ export const GithubState = ({children}) => {
         repos: []
     }
     const [state, dispatch] = useReducer(githubReducer, initialState);
+    const search = async value => {
+        setLoading();
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        })
+    }
+    const getUser = async name => {
+        setLoading();
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        })
+    }
+    const getRepos = async name => {
+        setLoading();
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        })
+    }
+    const clearUsers = () => dispatch({type: CLEAR_USERS})
+    const setLoading = () => dispatch({type: SET_LOADING})
+    const {user, users, repos, loading} = state;
     return (
         <GithubContext.Provider value={{
-
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
         }}>
             {children}
         </GithubContext.Provider>
