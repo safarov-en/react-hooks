2bd6855578abf99bf10753855de832753943fcd6
safarov-en <safarov.elkhan110@gmail.com>
2024-05-03 11:51:19 +0500

    Home page added

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+export const Search = () => {
+    return (
+        <div className="form-group">
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Введите ник пользователя..."
+            />
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
+import { Search } from "../components/Search"
+import { Card } from "../components/Card"
+
 export const Home = () => {
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <>
+            <Search />
+            <div className="row">
+                <div className="col-sm-4 mb-4">
+                    <Card />
+                </div>
+                <div className="col-sm-4 mb-4">
+                    <Card />
+                </div>
+                <div className="col-sm-4 mb-4">
+                    <Card />
+                </div>
+            </div>
+        </>
     )
 }
